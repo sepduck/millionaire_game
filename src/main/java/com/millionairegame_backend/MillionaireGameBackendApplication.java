b333e84dade8b28aa32b9074433398a1daff5777
sepduck <cutexinhzai2018@gmail.com>
2024-11-18 07:04:29 +0000

    first commit

---
+++ src/main/java/com/millionairegame_backend/MillionaireGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.millionairegame_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillionaireGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MillionaireGameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/millionairegame_backend/MillionaireGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millionairegame_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillionaireGameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
